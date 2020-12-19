d8ac7a7cac0ae9d33712e55a15074ee2e97df01f
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2020-12-19 22:45:13 +0900

    feat: lintのwarning解消

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,7 @@
 declare module '*.vue' {
+  // 直し方調査中のため、一旦ignore
+  // eslint-disable-next-line prettier/prettier
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<{}, {}, unknown>
   export default component
 }
